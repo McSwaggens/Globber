3ddc9af0f79dc601b573c97a205fb7315565487b
McSwaggens <McSwaggens@protonmail.com>
2016-08-08 17:01:19 +1000

    Added basic Game engine functions

--- Game.h
+++ Game.h
@@ -1,7 +1,15 @@
+#include "Entity.h"
+#include <list>
+
 #pragma once
 class Game
 {
+	std::list<Entity> entities;
+	void Tick();
 public:
+	bool Running;
 	Game();
+	void Run();
+	void RegisterEntity(Entity entity);
 };
 
