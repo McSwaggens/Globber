e3146db3f066b95f682e8c1f96f81538684f80f0
GamingBacon97 <billyB@yahoo.com>
2016-08-08 02:40:24 -0500

    addedlogic n shit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include <stdio>
+#include "SFMl/Graphics.hpp"
+#include "Entity.h"
 
-using namespace std;
 
-void main ()
-{
+int main() {
+	sf::Clock clock;
+	sf::RenderWindow MainWindow;
+	MainWindow.create(sf::VideoMode(1768, 992), "Anal Pong", sf::Style::Fullscreen);
+	MainWindow.setFramerateLimit(60);
+	while (MainWindow.isOpen()) {
+		//THIS IS WHERE ALL THE FACKIN GAME LOGIC GOES M8 YA FACKIN CUNT
+		double delta = clock.restart().asSeconds();
+	}
 }
