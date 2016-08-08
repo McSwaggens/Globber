d217de785628c0b388426387ef219b9a91348963
McSwaggens <McSwaggens@protonmail.com>
2016-08-08 17:05:07 +1000

    Added game and tick loop

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,21 @@
 #include "Game.h"
+#include <iostream>
 
 Game::Game() {
+}
 
+void Game::Run()
+{
+	while (Running)
+	{
+		Tick();
+	}
+}
+
+void Game::Tick()
+{
+	for (std::list<Entity>::iterator it = entities.begin(); it != entities.end(); it++)
+	{
+		it->Tick(1.0f);
+	}
 }
